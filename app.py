82eee0dbdd1b6846307312d0580923f049dbf199
tsojka <sojkasojkasojka@gmail.com>
2020-11-07 12:45:39 -0600

    working on converting scraping.py into a bunch of functions then into one scrape_all function.

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+#set up flask
+app = Flask(__name__)
+
+#use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# function will set up our scraping route. This route will be the 
+# "button" of the web application, the one that will scrape updated
+# data when we tell it to from the homepage of our web app 
+@app.route("/scrape")   # defines the route that Flask will be using.
+def scrape():
+   mars = mongo.db.mars  # assign a new variable that points to our Mongo database
+   mars_data = scraping.scrape_all()   #In this line, we're referencing the scrape_all function in the scraping.py
+   mars.update({}, mars_data, upsert=True) # update the database
+   return "Scraping Successful!"
+
+if __name__ == "__main__":
+   app.run()
---
+++ scraping.py
@@ -0,0 +1,70 @@
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+# Set the executable path and initilize the chrome browser in splinter
+ex_path = {'executable_path' : '/usr/local/bin/chromedriver'}
+browser = Browser('chrome', **ex_path, headless=True)
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+# set up html parser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+#find the parent class
+slide_elem.find("div", class_='content_title')
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+# # Featured Images
+
+# Visit URL
+url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_id('full_image')
+full_image_elem.click()
+
+# Find the more info button and click that
+browser.is_element_present_by_text('more info', wait_time=1)
+more_info_elem = browser.links.find_by_partial_text('more info')
+more_info_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# Find the relative image url
+img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+img_url_rel
+
+# Use the base URL to create an absolute URL
+img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+img_url
+
+# # Mars Facts
+
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.columns=['description', 'value']
+df.set_index('description', inplace=True)
+df
+
+# turn the df back into html code
+df.to_html()
+
+# quit the browser
+browser.quit()
